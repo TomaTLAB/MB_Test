5c8874d7ef689975d16223fc23f9511901b505c6
Anton Mironov <tomatlab@mail.ru>
2016-03-11 00:53:49 +0300

    check

--- MB_Test.ino
+++ MB_Test.ino
@@ -27,6 +27,7 @@ void setup() {
     // The IP address for the shield
     byte ip[] = { 192, 168, 1, 222 };
     //Config Modbus IP
+    SPI.setModule(2); // setting hardware SPI2 as default SPI module (pins 28, 29, 30 on maple mini)
     mb.config(mac, ip);
 
     // Config Modbus Serial (port, speed, byte format) 
@@ -43,10 +44,19 @@ void setup() {
     mb.addIsts(DPin);
     mb.addIreg(DPin);
     mb.addIreg(DPin + 4);
-    pinMode(DPin + 15, OUTPUT);
-    pinMode(DPin + 19, INPUT_PULLUP);
+    //pinMode(DPin + 15, OUTPUT);
+    //pinMode(DPin + 19, INPUT_PULLUP);
   }
-    pinMode(33, OUTPUT);
+    pinMode(PC13, OUTPUT);
+    pinMode(PB0, INPUT_ANALOG);
+    pinMode(PA0, INPUT_ANALOG);
+    pinMode(PA1, INPUT_ANALOG);
+    pinMode(PA2, INPUT_ANALOG);
+    pinMode(PA3, INPUT_ANALOG);
+    pinMode(PA4, INPUT_ANALOG);
+    pinMode(PA5, INPUT_ANALOG);
+    pinMode(PA6, INPUT_ANALOG);
+    pinMode(PA7, INPUT_ANALOG);
 
     mb.addCoil(13);
 
@@ -68,25 +78,54 @@ void loop()
    mb.task();
    //digitalWrite(2, false);
    
-   if (mb.Ireg(0) < 10)
+   if (mb.Ireg(0) < 1000)
    {
      mb.Coil(0, false);
      mb.Coil(13, false);
    }
-   if (mb.Ireg(0) > 1010)
+   if (mb.Ireg(0) > 2000)
    {
      mb.Coil(0, true);
      mb.Coil(13, true);
    }
 
-  for (byte APin = 0; APin < 8; APin++) 
-  {
-    mb.Ireg(APin, analogRead(7));
-  }
-  for (byte DPin = 0; DPin < 4; DPin++) 
-  {
-    mb.Ists(DPin, digitalRead(DPin + 19));
-   digitalWrite(DPin + 15, mb.Coil(DPin));
-  }
-   digitalWrite(33, mb.Coil(13));
+//  for (byte APin = 0; APin < 8; APin++) 
+//  {
+//    unsigned long buf = 0;
+//    for (byte cnt = 0; cnt < 10; cnt++)
+//      {
+//        buf += analogRead(APin);
+//      }
+//      mb.Ireg(APin, buf/10);
+//  }
+    mb.Ireg(0, analogRead(PA0));
+    mb.Ireg(0, analogRead(PA0));
+   mb.task();
+    mb.Ireg(1, analogRead(PA1));
+    mb.Ireg(1, analogRead(PA1));
+   mb.task();
+    mb.Ireg(2, analogRead(PA2));
+    mb.Ireg(2, analogRead(PA2));
+   mb.task();
+    mb.Ireg(3, analogRead(PA3));
+    mb.Ireg(3, analogRead(PA3));
+   mb.task();
+    mb.Ireg(4, analogRead(PA4));
+    mb.Ireg(4, analogRead(PA4));
+   mb.task();
+    mb.Ireg(5, analogRead(PA5));
+    mb.Ireg(5, analogRead(PA5));
+   mb.task();
+    mb.Ireg(6, analogRead(PA6));
+    mb.Ireg(6, analogRead(PA6));
+   mb.task();
+    mb.Ireg(7, analogRead(PA7));
+    mb.Ireg(7, analogRead(PA7));
+   mb.task();
+//  for (byte DPin = 0; DPin < 4; DPin++) 
+//  {
+//    mb.Ists(DPin, digitalRead(DPin + 16));
+//   digitalWrite(DPin + 12, mb.Coil(DPin));
+//  }
+   digitalWrite(PC13, mb.Coil(13));
 }
